56649c82d6e28a95f0dd7a7ce6ce46cae1fe6da4
Salman Ahmed <sscvocational4@gmail.com>
2024-12-13 22:24:39 +0600

    building the platform

--- script.js
+++ script.js
@@ -36,10 +36,48 @@ const scrollHeader = () =>{
 }
 
 window.addEventListener('scroll', scrollHeader())
-/*=============== SWIPER POPULAR ===============*/
+/*=============== Notification Adder ===============*/
 
+function addNotificationbar() {
+    const notificationBar = document.getElementById("notificationBar")
+    notificationBar.classList.toggle("show-notification")
+}
+function removeNotificationbar() {
+    const notificationBar = document.getElementById("notificationBar")
+    notificationBar.classList.remove("show-notification")
+}
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+// swiper
+const swiperEl = document.querySelector('swiper-container')
+    Object.assign(swiperEl, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        clickable: true,
+      },
+      breakpoints: {
+        "@0.00": {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        "@0.75": {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        "@1.00": {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        "@1.50": {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
+    });
+
+    swiperEl.initialize();
