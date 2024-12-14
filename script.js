3d4029447069775001f659602b0a86b01bc346e0
Salman Ahmed <sscvocational4@gmail.com>
2024-12-14 09:25:39 +0600

    last updated

--- script.js
+++ script.js
@@ -2,12 +2,10 @@
 const navMenu = document.getElementById("nav-menu");
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
-const navLink = document.querySelectorAll("#nav-link")
+const navLink = document.querySelectorAll("#nav-link");
 
 if (navToggle) {
-  navToggle.addEventListener("click", () =>
-    navMenu.classList.add("show-menu")
-  );
+  navToggle.addEventListener("click", () => navMenu.classList.add("show-menu"));
 }
 
 if (navClose) {
@@ -17,34 +15,33 @@ if (navClose) {
 
   /*=============== REMOVE MENU MOBILE ===============*/
   if (navLink) {
-    navLink.forEach(link => {
-        link.addEventListener("click",()=>{
-
-            navMenu.classList.remove("show-menu")
-
-        })
+    navLink.forEach((link) => {
+      link.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+      });
     });
-    
-}
+  }
 }
 
 /*=============== ADD SHADOW HEADER ===============*/
 
-const scrollHeader = () =>{
-  const header = document.getElementById('header')
-  this.scrollY >=50? header.classList.add('shadow-header') : header.classList.remove('shadow-header')
-}
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
 
-window.addEventListener('scroll', scrollHeader())
+window.addEventListener("scroll", scrollHeader());
 /*=============== Notification Adder ===============*/
 
 function addNotificationbar() {
-    const notificationBar = document.getElementById("notificationBar")
-    notificationBar.classList.toggle("show-notification")
+  const notificationBar = document.getElementById("notificationBar");
+  notificationBar.classList.toggle("show-notification");
 }
 function removeNotificationbar() {
-    const notificationBar = document.getElementById("notificationBar")
-    notificationBar.classList.remove("show-notification")
+  const notificationBar = document.getElementById("notificationBar");
+  notificationBar.classList.remove("show-notification");
 }
 /*=============== SHOW SCROLL UP ===============*/
 
@@ -53,31 +50,26 @@ function removeNotificationbar() {
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 // swiper
-const swiperEl = document.querySelector('swiper-container')
-    Object.assign(swiperEl, {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      pagination: {
-        clickable: true,
-      },
-      breakpoints: {
-        "@0.00": {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        "@0.75": {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        "@1.00": {
-          slidesPerView: 3,
-          spaceBetween: 40,
-        },
-        "@1.50": {
-          slidesPerView: 4,
-          spaceBetween: 50,
-        },
-      },
-    });
 
-    swiperEl.initialize();
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+  },
+});
